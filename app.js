42033b1f2f31c8e68a46dcaeb40bc5f57894bdac
Ordinary Probably <87839931+ahiou-1@users.noreply.github.com>
2021-10-28 15:21:31 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,20 +3,52 @@ const userMessage = document.querySelector('#msg');
 const chatContainer = document.querySelector('.chat-container');
 
 let messageId = 0;
+let chatHistory = [];
 
 chatForm.addEventListener('submit', (event) => {
-    postText(event);
+    getMessage(event);
     updateScroll();
+    console.log(chatHistory)
+    
 })
 
-function postText(event) {
+function displayMessage() {
+    chatContainer.textContent = '';
+    for(let i = 0; i < chatHistory.length; i++){
+        const chatDiv = document.createElement('div');
+        chatDiv.innerHTML = returnMessage(chatHistory[i]);
+        chatContainer.appendChild(chatDiv);
+    
+    }
+}
+
+function returnMessage(chatObj) {
+    return `
+        
+            <p>${chatObj.createdAt}</p>
+            <p>${chatObj.content}</p>
+        
+    `
+}
+
+function prettifyTime() {
+    //2021. 10. 28. 오후 2:31:03
+    let time = new Date().toLocaleString();
+    return `${time.slice(-8, -6)}시 ${time.slice(-5, -3)}분`
+}
+
+function getMessage(event) {
     event.preventDefault();
-    const message = msg.value;
+    let message = msg.value;
+
     if(message.includes('http://') || message.includes('https://')) {
         const linkFrontIndex = message.indexOf('http');
         let linkRearIndex;
+
+        // http 이후 첫 띄어쓰기가 없다면, url의 끝자리는 message의 끝자리다.
         if(message.indexOf(' ', linkFrontIndex) === -1) {
             linkRearIndex = message.lastIndexOf('');
+        // http 이후 띄어쓰기가 있다면, 그 위치가 url 의 끝자리다.
         } else {
             linkRearIndex = message.indexOf(' ', linkFrontIndex);
         }
@@ -24,32 +56,24 @@ function postText(event) {
         const frontText = message.slice(0, linkFrontIndex);
         const rearText = message.slice(linkRearIndex);
 
-        let messageBox = document.createElement('p');
-        messageBox.setAttribute('id', `${messageId}`)
-
-        let messageLink = document.createElement('a');
-        messageLink.setAttribute('href', `${hyperlink}`)
-        messageLink.innerText = `${hyperlink}`
-
-
-        // messageBox.textContent = `${message.slice(0, linkFrontIndex)+message.slice(linkRearIndex)}`;
-        messageBox.textContent = `${frontText}`;
-
-
-        messageId++;
-        chatContainer.appendChild(messageBox);
-        messageBox.appendChild(messageLink);
-        messageBox.textContent += `${rearText}`
-        userMessage.value = '';
-        return;
+        message = `${frontText} <a href=${hyperlink}>${hyperlink}</a> ${rearText}`
     }
-    let messageBox = document.createElement('p');
-    messageBox.setAttribute('id', `${messageId}`)
-    messageBox.textContent = `${message}`;
-    messageId++;
-    chatContainer.appendChild(messageBox);
-    userMessage.value = '';
+
+    createChatObj(message);
+    msg.value = '';
 }
+
+function createChatObj(message) {
+    const chatObj = {
+        type: 'text',
+        content: message,
+        createdAt: prettifyTime()
+    }
+    chatHistory.unshift(chatObj);
+    displayMessage();
+} 
+
+
 
 function updateScroll() {
     chatContainer.scrollTop = chatContainer.scrollHeight;
